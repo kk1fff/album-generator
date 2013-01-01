a8656ac1a3ea1052d946560ca3a623ef9699b9b8
kk1fff <kk1fff@patrickz.net>
2013-01-01 18:10:59 +0800

    Add new year in license. HAPPY NEW YEAR!!

--- template-interface.js
+++ template-interface.js
@@ -1,4 +1,4 @@
-// Copyright 2012 Patrick Wang <kk1fff@patrickz.net>
+// Copyright 2012, 2013 Patrick Wang <kk1fff@patrickz.net>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
